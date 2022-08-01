f5a4b47103a6aed5a55ef0bf5723389c8cc76758
Lucy Davinhart <lucy@lucymhdavies.com>
2022-08-01 17:27:07 +0100

    Use Doormat for creds

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   cloud {
-    organization = "lmhd"
+    organization = "hashi_strawb_demo"
 
     workspaces {
       name = "demo-tfcb-drift"
@@ -11,11 +11,37 @@ terraform {
     aws = {
       source = "hashicorp/aws"
     }
+
+    doormat = {
+      source = "doormat.hashicorp.services/hashicorp-security/doormat"
+    }
   }
 }
 
+provider "doormat" {}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+  role_arn = "arn:aws:iam::711129375688:role/strawb-tfc-hashi_strawb_demo-${terraform.workspace}"
+}
+
 provider "aws" {
-  region = "eu-west-2"
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
+  region     = "us-east-1" # doesn't matter, but required by the provider
+
+  default_tags {
+    tags = {
+      Name      = "StrawbTest"
+      Owner     = "lucy.davinhart@hashicorp.com"
+      Purpose   = "Demonstrating Drift Detection"
+      TTL       = "24h"
+      Terraform = "true"
+      Source    = "https://github.com/hashi-strawb/demo-tfcb-drift"
+      Workspace = terraform.workspace
+    }
+  }
 }
 
 data "aws_caller_identity" "current" {}
